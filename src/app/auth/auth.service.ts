b7e8ac2a2283b28bc22fdbdc350cd3e78c73e48c
riefky-bayu-zetta <57337920+riefky-bayu-zetta@users.noreply.github.com>
2021-11-16 20:29:54 +0800

    Setup Login Page

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
-
+import { Apollo, gql } from 'apollo-angular';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private apollo: Apollo) { }
+
+  loginUser(email: string, password: string): Observable<any> {
+    return this.apollo.mutate({
+      mutation: gql`
+        mutation {
+          Login(email: "${email}", password: "${password}") {
+            token
+          }
+        }
+      `
+      }).pipe(
+        map((resp) => {
+          this.userLogin(resp);
+          return resp;
+        }),
+      );
+  }
+
+  userLogin(resp: any) {
+    localStorage.setItem(environment.tokenKey, JSON.stringify(resp.token));
+  }
+
+  logOut() {
+    localStorage.removeItem(environment.tokenKey);
+  }
 }
--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/auth/auth.service';
+import { SubSink } from 'subsink';
 
+
+interface Payload {
+  email: string,
+  password: string
+}
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  private subs = new SubSink();
+  loginForm: FormGroup = this.initFormGroup();
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  initFormGroup() {
+    return this.fb.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    })
+  }
+
+  login() {
+    const payload: Payload = this.loginForm.value;
+    this.subs.sink = this.authService.loginUser(payload.email, payload.password).subscribe(resp => {
+      console.log(resp);
+    })
   }
 
 }
