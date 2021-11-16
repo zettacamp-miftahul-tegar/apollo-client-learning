b18afc73a6edd9fe4b2790d6393ec0a460d725ff
riefky-bayu-zetta <57337920+riefky-bayu-zetta@users.noreply.github.com>
2021-11-16 17:43:09 +0800

    Create auth service

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRoute, ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+  ) {}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+      const expectedPermission = route.data.permission;
+      const userToken = localStorage.getItem('token');
+      // const userData = JSON.parse(localStorage.getItem('userProfile'));
+
+      
+      return true;
   }
   
 }
